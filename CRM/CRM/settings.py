7561e9cae13af39c5c5087f08a7e94180fdc02df
Kmenguete <menguetekevin@gmail.com>
2023-01-11 14:00:30 +0100

    installing and configuring a postgresql database

--- CRM/CRM/settings.py
+++ CRM/CRM/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'CRM.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'crmDB',
+        'USER': 'kevin',
+        'PASSWORD': 'dammuni',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
