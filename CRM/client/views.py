8e12471da1102461b2dececa660ec2527d54f4cb
Kmenguete <menguetekevin@gmail.com>
2023-01-13 14:25:23 +0100

    adding allowed methods for clients objects

--- CRM/client/views.py
+++ CRM/client/views.py
@@ -5,8 +5,17 @@ from CRM.client.serializers import ClientSerializer
 
 
 class ClientViewSet(ModelViewSet):
-
+    http_method_names = ["get", "post", "put"]
     serializer_class = ClientSerializer
 
     def get_queryset(self):
         return Client.objects.all()
+
+    def create(self, request, *args, **kwargs):
+        request.POST._mutable = True
+        request.data["sales_contact"] = request.user.pk
+        request.POST._mutable = False
+        return super(ClientViewSet, self).create(request, *args, **kwargs)
+
+    def perform_update(self, serializer):
+        serializer.save(sales_contact=self.request.user)
