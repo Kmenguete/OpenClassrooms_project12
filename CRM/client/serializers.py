4de61a422086ed9180315b8cce3e21e39e827009
Kmenguete <menguetekevin@gmail.com>
2023-01-13 13:44:09 +0100

    creating serializer for client model

---
+++ CRM/client/serializers.py
@@ -0,0 +1,12 @@
+from rest_framework.serializers import ModelSerializer
+
+from CRM.client.models import Client
+
+
+class ClientSerializer(ModelSerializer):
+
+    class Meta:
+        model = Client
+        fields = ['id', 'first_name', 'last_name',
+                  'email', 'phone', 'mobile', 'company_name',
+                  'date_created', 'date_updated', 'sales_contact']
