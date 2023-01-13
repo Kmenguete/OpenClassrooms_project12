57bd8203d3f85384d0a4d0afd8340b2428d7b232
Kmenguete <menguetekevin@gmail.com>
2023-01-13 11:34:17 +0100

    creating client model

--- CRM/client/models.py
+++ CRM/client/models.py
@@ -1,3 +1,14 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+
+class Client(models.Model):
+    first_name = models.CharField(max_length=128)
+    last_name = models.CharField(max_length=128)
+    email = models.EmailField()
+    phone = models.CharField(max_length=128)
+    mobile = models.CharField(max_length=128)
+    company_name = models.CharField(max_length=128)
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_updated = models.DateTimeField(auto_now_add=True)
+    sales_contact = models.ForeignKey(to=settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
