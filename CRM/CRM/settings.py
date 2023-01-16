e9df68cf26efddbc35244b4fec4796d50f6c783e
Kmenguete <menguetekevin@gmail.com>
2023-01-16 13:44:47 +0100

    installing contract app and creating a Contract model

--- CRM/CRM/settings.py
+++ CRM/CRM/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'authentication',
     'client',
     'rest_framework',
+    'contract',
 ]
 
 MIDDLEWARE = [
---
+++ CRM/contract/models.py
@@ -0,0 +1,14 @@
+from django.conf import settings
+from django.db import models
+
+from client.models import Client
+
+
+class Contract(models.Model):
+    sales_contact = models.ForeignKey(to=settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    client = models.ForeignKey(to=Client, on_delete=models.CASCADE)
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_updated = models.DateTimeField(auto_now_add=True)
+    status = models.BooleanField(default=True, blank=True)
+    amount = models.FloatField()
+    payment_due = models.DateTimeField(auto_now_add=True)
