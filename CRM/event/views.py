9bcd3c9727ca7fff10832d9dd9d4b8fac778b4e4
Kmenguete <menguetekevin@gmail.com>
2023-01-20 15:36:40 +0100

    preventing a support contact to create an event

--- CRM/event/views.py
+++ CRM/event/views.py
@@ -1,3 +1,4 @@
+from rest_framework.exceptions import PermissionDenied
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.viewsets import ModelViewSet
 
@@ -7,11 +8,17 @@ from .serializers import EventSerializer
 
 class EventViewSet(ModelViewSet):
     permission_classes = [IsAuthenticated]
-    http_method_names = ["get", "put"]
+    http_method_names = ["get", "post", "put"]
     serializer_class = EventSerializer
 
     def get_queryset(self):
         return Event.objects.all()
+
+    def create(self, request, *args, **kwargs):
+        if request.user.role != "Sales Contact":
+            raise PermissionDenied
+        else:
+            return super(EventViewSet, self).create(request, *args, **kwargs)
 
     def perform_update(self, serializer):
         serializer.save(sales_contact=self.request.user)
