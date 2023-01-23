1b4bb2c3178f754e500f0f99c452ce12be7d34fe
Kmenguete <menguetekevin@gmail.com>
2023-01-23 17:01:57 +0100

    I want to understand why email is None in my custom authentication

--- CRM/authentication/authentication.py
+++ CRM/authentication/authentication.py
@@ -5,14 +5,17 @@ from .models import CustomUser
 
 class MyAuthentication(authentication.BaseAuthentication):
     def authenticate(self, request):
-        email = request.META.get('email')
-        password = request.META.get('password')
-        user = CustomUser.objects.get(email=email)
-        if not email or not password:
-            raise exceptions.AuthenticationFailed('No credentials provided.')
-        elif email or password is None:
-            raise exceptions.AuthenticationFailed('Invalid username/password.')
-        elif not user.is_active:
-            raise exceptions.AuthenticationFailed('User inactive or deleted.')
+        email = request.data.get('email', None)
+        password = request.data.get('password', None)
+        try:
+            user = CustomUser.objects.get(email=email)
+            if not email or not password:
+                raise exceptions.AuthenticationFailed('No credentials provided.')
+            elif email or password is None:
+                raise exceptions.AuthenticationFailed('Invalid username/password.')
+            elif not user.is_active:
+                raise exceptions.AuthenticationFailed('User inactive or deleted.')
+        except CustomUser.DoesNotExist:
+            raise exceptions.AuthenticationFailed(f"The user with email {email} does not exists.")
 
         return (user, None)
