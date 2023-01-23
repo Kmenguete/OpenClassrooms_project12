e0bc6364cf783196dc53ccb5aecdb869c5c08c99
Kmenguete <menguetekevin@gmail.com>
2023-01-23 16:46:36 +0100

    Now I have to understand why my user is not found?

--- CRM/authentication/authentication.py
+++ CRM/authentication/authentication.py
@@ -7,10 +7,12 @@ class MyAuthentication(authentication.BaseAuthentication):
     def authenticate(self, request):
         email = request.META.get('email')
         password = request.META.get('password')
-        if not email and password:
-            return None
-        try:
-            user = CustomUser.objects.get(email=email)
-        except CustomUser.DoesNotExist:
-            raise exceptions.AuthenticationFailed('No such user')
+        user = CustomUser.objects.get(email=email)
+        if not email or not password:
+            raise exceptions.AuthenticationFailed('No credentials provided.')
+        elif email or password is None:
+            raise exceptions.AuthenticationFailed('Invalid username/password.')
+        elif not user.is_active:
+            raise exceptions.AuthenticationFailed('User inactive or deleted.')
+
         return (user, None)
