4c007a6b99d562559090b96bbd7fb5efc6f83851
Kmenguete <menguetekevin@gmail.com>
2023-01-27 15:41:33 +0100

    successfully setting the author of the client object as the logged in user

--- CRM/client/serializers.py
+++ CRM/client/serializers.py
@@ -10,3 +10,10 @@ class ClientSerializer(ModelSerializer):
         fields = ['id', 'first_name', 'last_name',
                   'email', 'phone', 'mobile', 'company_name',
                   'date_created', 'date_updated', 'sales_contact']
+        read_only_fields = ['sales_contact']
+
+    def create(self, validated_data):
+        user = self.context["request"].user
+        client = Client.objects.create(**validated_data, sales_contact=user)
+
+        return client
