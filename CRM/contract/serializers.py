16b9b87a8c10f8ee43eb3f4b7d077ba39c688e45
Kmenguete <menguetekevin@gmail.com>
2023-01-30 09:44:22 +0100

    Sales contact should be able to create a contract only for their own clients

--- CRM/contract/serializers.py
+++ CRM/contract/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework.serializers import ModelSerializer
 
 from .models import Contract
+from client.models import Client
 
 
 class ContractSerializer(ModelSerializer):
@@ -11,3 +12,12 @@ class ContractSerializer(ModelSerializer):
                   "client", "date_created",
                   "date_updated", "status",
                   "amount", "payment_due"]
+
+        read_only_fields = ['sales_contact']
+
+        def create(self, validated_data):
+            user = self.context["request"].user
+            contract = Contract.objects.create(**validated_data, sales_contact=user)
+            clients = Client.object.filter(**validated_data, sales_contact=user)
+
+            return contract, clients
