c2109100752bc1ebd10d3e2927feca02f496c09e
Kmenguete <menguetekevin@gmail.com>
2023-01-31 13:52:13 +0100

    trying to understand why a support contact cannont fill client and event status field when updating an event

--- CRM/event/permissions.py
+++ CRM/event/permissions.py
@@ -1,21 +1,12 @@
 from rest_framework.permissions import BasePermission, SAFE_METHODS
 
 
-class IsSalesContact(BasePermission):
+class IsSupportContact(BasePermission):
 
     def has_object_permission(self, request, view, obj):
         if request.method in SAFE_METHODS:
             return True
         elif request.method in ('POST',):
-            return True
-        else:
-            return obj.support_contact == request.user
-
-
-class IsSupportContact(BasePermission):
-
-    def has_object_permission(self, request, view, obj):
-        if request.method in ('PUT',):
             return True
         else:
             return obj.support_contact == request.user
