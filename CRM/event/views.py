5698e180e1b23467092fd78ba98fbd3cff59a427
Kmenguete <menguetekevin@gmail.com>
2023-02-01 16:57:01 +0100

    Unable to compare dates

--- CRM/event/views.py
+++ CRM/event/views.py
@@ -4,13 +4,13 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.viewsets import ModelViewSet
 from django_filters.rest_framework import DjangoFilterBackend
 from .models import Event
-from .permissions import IsSalesContact, IsSupportContact
+from .permissions import IsSalesContact, IsSupportContact, PastDate
 from .serializers import EventSerializer, SupportEventSerializer
 from client.models import Client
 
 
 class EventViewSet(ModelViewSet):
-    permission_classes = [IsAuthenticated, IsSalesContact]
+    permission_classes = [IsAuthenticated, IsSalesContact, PastDate]
     http_method_names = ["get", "post"]
     serializer_class = EventSerializer
     filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
@@ -38,7 +38,7 @@ class EventViewSet(ModelViewSet):
 
 
 class SupportEventViewSet(ModelViewSet):
-    permission_classes = [IsAuthenticated, IsSupportContact]
+    permission_classes = [IsAuthenticated, IsSupportContact, PastDate]
     http_method_names = ["get", "put"]
     serializer_class = SupportEventSerializer
     filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
